0af2cc5c72bd49c6d3d7590ef77f8e8d54ef9f0c
Bheny <Baronbenny23@gmail>
2023-07-06 15:35:55 +0000

    initialized project

---
+++ api/engine/serializers.py
@@ -0,0 +1,3 @@
+from drf_base64.fields import Base64ImageField
+from rest_framework import serializers
+
---
+++ api/engine/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+
+# Wire up our API using automatic URL routing.
+urlpatterns = [
+
+]
